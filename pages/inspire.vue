ca1cbf4d16d14babd3891b4b5abfac37a712e5e0
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-06-17 10:48:34 -0500

    set dashboard layout

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,11 +1,7 @@
 <template>
   <v-layout>
     <v-flex class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
         <footer>
@@ -17,3 +13,8 @@
     </v-flex>
   </v-layout>
 </template>
+<script>
+export default {
+  layout: 'dashboard'
+}
+</script>
